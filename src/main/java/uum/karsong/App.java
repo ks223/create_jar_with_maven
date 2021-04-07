b5665a1398991205223b5c9bec3758999558d409
ks223 <ngkarsong@gmail.com>
2021-04-07 12:22:58 +0800

    first commit

---
+++ src/main/java/uum/karsong/App.java
@@ -0,0 +1,19 @@
+package uum.karsong;
+
+import javax.swing.*;
+
+public class App extends JFrame
+{
+    public App() {
+        setTitle("Hello World");
+        setSize(688,488);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+    public static void main( String[] args )
+    {
+        new App();
+    }
+}
